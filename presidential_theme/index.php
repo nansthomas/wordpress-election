abad5f430fef2c6ee1d2810233f016ee48488337
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-11 14:15:51 +0100

    menu

--- presidential_theme/index.php
+++ presidential_theme/index.php
@@ -2,11 +2,7 @@
 
 <div class="content">
     <h1>hello</h1>
-    <?php while ( have_posts() ) : the_post(); ?> 
     <?php the_content(); ?>
 </div> <!-- /content -->
-    <?php
-        endwhile; //resetting the page loop
-        wp_reset_query(); //resetting the page query
-    ?>
+
 <?php get_footer(); ?>
