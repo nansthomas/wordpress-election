730c41a9801365c5a1658f40f4258ee53e3da42e
Clément Vion <clement.vion1@gmail.com>
2016-11-11 15:46:22 +0100

    Add scripts/styles

--- presidential_theme/functions.php
+++ presidential_theme/functions.php
@@ -4,3 +4,4 @@ include_once ('includes/theme_support.php');
 include_once ('includes/custom_post_types.php');
 include_once ('includes/roles.php');
 include_once ('includes/woocommerce.php');
+include_once ('includes/scripts_styles.php');
