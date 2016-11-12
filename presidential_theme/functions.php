f64debacda6317dc2ad8c4c4ee0e90b5924961f8
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-12 17:41:06 +0100

    [FUNCTIONS] added excerpt function to trim down the excerpt nicely

--- presidential_theme/functions.php
+++ presidential_theme/functions.php
@@ -3,6 +3,7 @@
 include_once ('includes/theme_support.php');
 include_once ('includes/custom_post_types.php');
 include_once ('includes/roles.php');
+include_once ('includes/excerpt.php');
 include_once ('includes/sidebars.php');
 include_once ('includes/taxonomies.php');
 include_once ('includes/woocommerce.php');
---
+++ presidential_theme/includes/excerpt.php
@@ -0,0 +1,13 @@
+<?php 
+
+function get_excerpt(){
+$excerpt = get_the_content();
+$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+$excerpt = strip_shortcodes($excerpt);
+$excerpt = strip_tags($excerpt);
+$excerpt = substr($excerpt, 0, 50);
+$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+$excerpt = trim(preg_replace( '/s+/', ' ', $excerpt));
+$excerpt = $excerpt.'... <a href="'.$permalink.'">more</a>';
+return $excerpt;
+}
