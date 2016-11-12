cdd377ae69058306beef479decb2f8321002e41c
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-12 17:59:27 +0100

    [FUNCTIONS] Modified the excerpts length

--- presidential_theme/includes/excerpt.php
+++ presidential_theme/includes/excerpt.php
@@ -6,7 +6,7 @@ $excerpt = get_the_content();
 $excerpt = preg_replace(" ([.*?])",'',$excerpt);
 $excerpt = strip_shortcodes($excerpt);
 $excerpt = strip_tags($excerpt);
-$excerpt = substr($excerpt, 0, 50);
+$excerpt = substr($excerpt, 0, 218);
 $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
 $excerpt = trim(preg_replace( '/s+/', ' ', $excerpt));
 $excerpt = $excerpt.'... <a href="'.$permalink.'">more</a>';
