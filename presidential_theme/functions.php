7c7f8a0de1e1de541674574bd400c0329ce1bdcc
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-12 14:57:35 +0100

    [TAXONOMIES] added news tag taxonomy

--- presidential_theme/functions.php
+++ presidential_theme/functions.php
@@ -4,5 +4,6 @@ include_once ('includes/theme_support.php');
 include_once ('includes/custom_post_types.php');
 include_once ('includes/roles.php');
 include_once ('includes/sidebars.php');
+include_once ('includes/taxonomies.php');
 include_once ('includes/woocommerce.php');
 include_once ('includes/scripts_styles.php');
