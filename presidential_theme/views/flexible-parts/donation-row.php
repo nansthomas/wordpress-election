d54ac375ce8bb2298c624e3c6da2d3379706bd97
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-13 11:34:02 +0100

    [PAGES] fixed donation row on home-page

---
+++ presidential_theme/views/flexible-parts/donation-row.php
@@ -0,0 +1,22 @@
+<?php
+	echo 'oooo';
+	$donation_title = get_sub_field('texte_don');
+	$first_amount = get_sub_field('premier_montant');
+	$second_amount = get_sub_field('deuxieme_montant');
+	$third_amount = get_sub_field('troisieme_montant');
+	$fourth_amount = get_sub_field('quatrieme_montant');
+?>
+
+<section class="donationFormMin">
+
+	<div class="donationFormMin__container">
+		<h3 class="donationFormMin__title"><?php echo $donation_title; ?></h3>
+		<ul class="donationFormMin__amounts">
+			<li class="donationFormMin__amount"><?php echo $first_amount; ?>€</li>
+			<li class="donationFormMin__amount"><?php echo $second_amount; ?>€</li>
+			<li class="donationFormMin__amount"><?php echo $third_amount; ?>€</li>
+			<li class="donationFormMin__amount"><?php echo $fourth_amount; ?>€</li>
+		</ul>
+	</div>
+
+</section>
