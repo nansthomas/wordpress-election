b13e531d8467ada4ea2b879a8ac319f2e12da5db
Nans THOMAS <nans.thomas@hetic.net>
2016-11-14 12:00:36 +0100

    Debug SetAttribute for Stripe

--- presidential_theme/views/flexible-parts/bande_don.php
+++ presidential_theme/views/flexible-parts/bande_don.php
@@ -16,10 +16,10 @@
 			<li class=""><?php echo $second_amount; ?></li>
 			<li class=""><?php echo $third_amount; ?></li>
 			<li class=""><?php echo $fourth_amount; ?></li>
-			<li class="custom"><input type="number" class="custom_price" name="" value=""><a class="ok_button">OK</a></li>
+			<li class="custom"><input type="number" class="custom_price" name="" value=""></li>
       <form action="/" method="POST">
         <script
-          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          src="https://checkout.stripe.com/checkout.js" class="stripe-button custom-stripe"
           data-key="pk_test_vWGaBesvcQvmQpkJYZ1BSI9g"
           data-amount="2000"
           data-name="Nansthomas"
@@ -35,14 +35,14 @@
 </section>
 
 <script type="text/javascript">
-  var custom_form_script = document.querySelector('.stripe-button');
+  var custom_form_script = document.querySelector('.custom-stripe');
   var custom_price = document.querySelector('.custom_price');
 
   var toto = custom_form_script.getAttribute('data-amount');
   console.log(toto);
 
   custom_price.addEventListener('input', function () {
-    custom_form_script.setAttribute('data-amount', custom_price.value);
+    custom_form_script.setAttribute('data-amount', custom_price.value*100);
     console.log('attribue :' + toto);
     console.log('input :' + custom_price.value);
   });
