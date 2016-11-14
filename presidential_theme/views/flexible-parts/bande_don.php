ba9d625277938dd4f51275a9accae243d08f7c91
Nans THOMAS <nans.thomas@hetic.net>
2016-11-14 10:37:03 +0100

    Remove hard code & set plugin

--- presidential_theme/views/flexible-parts/bande_don.php
+++ presidential_theme/views/flexible-parts/bande_don.php
@@ -12,19 +12,6 @@
 	<div class="donationFormMin__container">
 		<h3 class="donationFormMin__title"><?php echo $donation_title; ?></h3>
 		<ul class="donationFormMin__amounts">
-      <form action="/your-server-side-code" method="POST">
-        <script
-          src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-          data-key="pk_test_nYVHsex9YtLnndKBp6HmxyAN"
-          data-amount="2000"
-          data-name="Wino Technologies SAS"
-          data-description="2 widgets"
-          data-image="https://s3.amazonaws.com/stripe-uploads/acct_18f3GOLrz9UfSkFimerchant-icon-1472593676866-Wino_Application.png"
-          data-locale="auto"
-          data-zip-code="true"
-          data-currency="eur">
-        </script>
-      </form>
 			<li class="donationFormMin__amount"><?php echo $first_amount; ?>€</li>
 			<li class="donationFormMin__amount"><?php echo $second_amount; ?>€</li>
 			<li class="donationFormMin__amount"><?php echo $third_amount; ?>€</li>
