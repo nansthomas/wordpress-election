e916de521683dd4f7ed5e3bd146f49e7a162185d
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-15 18:05:06 +0100

    finished single-new template

--- presidential_theme/single-new.php
+++ presidential_theme/single-new.php
@@ -4,28 +4,25 @@
 	if ( have_posts() ) {
 		while(have_posts() ) {
 			the_post();
-?>
+			if(has_post_thumbnail())
+			{ ?>
+				<section class="largeHeader" style="background-image: url(' <?php the_post_thumbnail("thumbnail_new_full"); ?> ')">';
+			<?php } ?>
 
-			<?php
-				if(has_post_thumbnail())
-				{
-					echo '<section class="largeHeader" style="background-image: url('. the_post_thumbnail("thumbnail_new_full") .')">';
-				}
-			?>
 <!-- set up the main image of the article in the attribute style -->
     
-    <div class="largeHeader__content">
-		<h1 class="largeHeader__title"><?php the_title(); ?></h1>
-		<h3 class="largeHeader__subtitle"><?php get_field('subtitle'); ?></h3>
-		<h4><?php the_time('j F Y'); ?></h4>
-	</div>
+			    <div class="largeHeader__content">
+					<h1 class="largeHeader__title"><?php the_title(); ?></h1>
+					<h3 class="largeHeader__subtitle"><?php get_field('subtitle'); ?></h3>
+					<h4><?php the_time('j F Y'); ?></h4>
+				</div>
 
 
-</section>
+			</section>
 
-<div class="articleContent"><?php the_content(); ?></div>
+			<div class="articleContent"><?php the_content(); ?></div>
 <?php
-			}
 		}
-	?>
+	}
+?>
 <?php get_footer(); ?>
