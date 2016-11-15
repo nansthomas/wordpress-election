21d3e8eb23940a1b14d4c23d8a23584a8dee052f
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-15 21:29:23 +0100

    added dynamic image on product

--- presidential_theme/woocommerce/archive-product.php
+++ presidential_theme/woocommerce/archive-product.php
@@ -28,8 +28,8 @@ if ( have_posts() ) {
 			echo "while have_post ok";
 			the_post();
 			var_dump(the_post());
-			$image = get_field('image_boutique');
-
+			$image = the_field('image_boutique', get_option('Boutique'));
+			var_dump($image);
 			if( !empty($image_boutique) ): ?>
 ?>
 	
