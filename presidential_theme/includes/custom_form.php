c2482e3f99cd131dfed0b66b20b63d287d1d04b9
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-16 23:43:29 +0100

    Added billet send mail

--- presidential_theme/includes/custom_form.php
+++ presidential_theme/includes/custom_form.php
@@ -5,8 +5,8 @@ function new_user_acf(){
 
 	// Pour ma part j’envoi un e-mail de bienvenue que je récupère ci-dessous
 	$content = 'zzzzzzzzz';
-	$headers = 'From: Monsite' . '\r\n';
-	$headers .= 'MIME-Version: 1.0' . '\r\n';
-	$headers .= 'Content-Type: text/html; charset=uft-8'.'\r\n';
-	wp_mail('arnaud.villani@gmail.com', 'Bienvenue sur Mon site', $content, $headers);
+	$headers = 'From: Monsite' . \r\n;
+	$headers .= 'MIME-Version: 1.0' . \r\n;
+	$headers .= 'Content-Type: text/html; charset=uft-8'. \r\n;
+	wp_mail('arnaud.villani@gmail.com', 'Bienvenue sur Mon site', 'orfidooi', $headers);
 }
