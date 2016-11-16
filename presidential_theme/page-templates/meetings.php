4da0edcaeff8f64e9aa5c52f05f07e2762307237
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-16 18:10:53 +0100

    Added meetings custom fields + meetings page template

--- presidential_theme/page-templates/meetings.php
+++ presidential_theme/page-templates/meetings.php
@@ -2,4 +2,25 @@
 	/*
 	Template Name: Page meetings
 	*/
+
+ 	$args = array(
+		'post_type' => 'meeting',
+		);
+
+ 	$the_query = new WP_Query( $args );
+
+	if ( have_posts() ) {
+		while(have_posts() ) {
+			the_post();
+?>
+	<div class="meeting">
+		<h1><?php the_field('ville'); ?></h1>
+		<h2><?php the_field('date'); ?></h2>
+		<a href="<?php the_permalink(); ?>">voir</a>
+	</div>
+
+<?php 
+		}	
+	}
+
 ?>
