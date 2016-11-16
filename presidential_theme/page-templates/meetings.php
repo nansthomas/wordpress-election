e464115079c957547613e0c3f7fe2c7a8f7eae48
Clément Vion <clement.vion1@gmail.com>
2016-11-16 18:38:04 +0100

    [LAYOUT] meetings

--- presidential_theme/page-templates/meetings.php
+++ presidential_theme/page-templates/meetings.php
@@ -13,7 +13,7 @@
 		while($the_query->have_posts() ) {
 			$the_query->the_post();
 ?>
-	<div class="meeting">
+	<div class="blogMeeting">
 		<?php
 			if(has_post_thumbnail())
 			{	
@@ -34,3 +34,42 @@
 	}
 
 ?>
+
+<section class="templateBlog">
+    
+    <div class="templateBlog__container">
+    	
+		<?php 
+		$args = array(
+			'post_type' => 'meeting',
+		);
+
+		 	$the_query = new WP_Query( $args );
+
+			if ( $the_query->have_posts() ) {
+				while($the_query->have_posts() ) {
+					$the_query->the_post();
+		?>
+		<article class="templateBlog__article">
+			<?php
+				if(has_post_thumbnail())
+				{	
+					echo '<div class="img">';
+					the_post_thumbnail("thumbnail_meetingpage");
+					echo '</div>';
+				}
+			?>
+			<h5 class="templateBlog__articleTitle"><?php the_title(); ?></h5>
+			<div class="templateBlog__articleDescription">
+				<?php echo get_excerpt(); ?>
+			</div>
+		</article>
+		<?php 
+			}
+			// restore original post data
+			wp_reset_postdata();
+		}
+		?>
+	</div>
+
+</section>
