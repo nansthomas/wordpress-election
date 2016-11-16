1689ddd4c0416feed7c79fcdc65c1ed91d416762
Clément Vion <clement.vion1@gmail.com>
2016-11-16 18:39:35 +0100

    [LAYOUT] meetings

--- presidential_theme/page-templates/meetings.php
+++ presidential_theme/page-templates/meetings.php
@@ -3,42 +3,12 @@
 	Template Name: Page meetings
 	*/
 
- 	$args = array(
-		'post_type' => 'meeting',
-		);
-
- 	$the_query = new WP_Query( $args );
-
-	if ( $the_query->have_posts() ) {
-		while($the_query->have_posts() ) {
-			$the_query->the_post();
+	get_header(); ?>
 ?>
-	<div class="blogMeeting">
-		<?php
-			if(has_post_thumbnail())
-			{	
-				echo '<div class="img">';
-				the_post_thumbnail("thumbnail_meetingpage");
-				echo '</div>';
-			}
-		?>
-		<h1><?php the_field('ville'); ?></h1>
-		<h2><?php the_field('date'); ?></h2>
-		<a href="<?php the_permalink(); ?>">voir</a>
-	</div>
-
-<?php 
-		}
-		wp_reset_postdata();
-	
-	}
-
-?>
-
 <section class="templateBlog">
     
     <div class="templateBlog__container">
-    	
+
 		<?php 
 		$args = array(
 			'post_type' => 'meeting',
@@ -73,3 +43,5 @@
 	</div>
 
 </section>
+
+<?php get_footer(); ?>
