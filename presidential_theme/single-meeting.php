0d6f170b11f7cffa26bd23db88f49cc79145582a
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-16 18:34:38 +0100

    Added single-meeting template

--- presidential_theme/single-meeting.php
+++ presidential_theme/single-meeting.php
@@ -15,7 +15,7 @@
 					<h1 class="largeHeader__title"><?php the_field('ville')?></h1>
 					<h3 class="largeHeader__subtitle"><?php the_field('date'); ?></h3>
 					<h4><?php the_field('adresse'); ?></h4>
-					<h5><?php the_field('heure');</h5>
+					<h5><?php the_field('heure'); ?></h5>
 				</div>
 
 
