3148963dca7a060abdc880fc880b2d95890ffde7
Clément Vion <clement.vion1@gmail.com>
2016-11-17 20:56:37 +0100

    style meetings

--- presidential_theme/header.php
+++ presidential_theme/header.php
@@ -29,7 +29,7 @@
                     ?>
                 </div>
                 
-                <img src="<?php echo home_url( 'wp-content/themes/presidential_theme/img/logo.png' ); ?>" alt="" class="log">
+                <img src="<?php echo home_url( 'wp-content/themes/presidential_theme/img/logo.png' ); ?>" alt="" class="logo">
             </header>
 
             <!-- <div class="container"> -->
