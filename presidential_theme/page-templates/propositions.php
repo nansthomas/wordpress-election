087a8c0d282db4b14888755bc0bd8254b86a8ab0
Clément Vion <clement.vion1@gmail.com>
2016-11-17 17:38:15 +0100

    style propositions

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -37,7 +37,7 @@ $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
 
 				// Get the taxonomy for the post
 				the_terms( get_the_ID(), 'categorie-proposition');
-				echo 'hienfkmk';
+				
 	    		// Display proposition
 	    		the_title();
 	    		echo "</li>";
