1bfd4fe796f61a047a31980c96319882a67d2963
Clément Vion <clement.vion1@gmail.com>
2016-11-17 16:57:45 +0100

    fix li propositions page

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -5,6 +5,11 @@
 
 get_header();	
 
+?>
+
+<h1 class="propositions__title">L'ensemble des propositions</h1>
+
+<?php
 $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
     if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
 	    echo '<ul class="propositions__choice">';
@@ -26,6 +31,15 @@ $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
 		if ( $the_query->have_posts() ) {
 			while( $the_query->have_posts() ) {
 				$the_query->the_post();
+
+				$terms = get_the_terms($post->id, 'category');
+			    $count = count( $terms );
+			    var_dump($term);
+			    if ( $count > 0 ) {
+			        foreach ( $terms as $term ) {
+			            echo '<li>' . $term->name . '</li>';
+			        }
+			    }
 	    		echo "<li class='propositionsList__item'>";
 	    		the_title();
 	    		echo "</li>";
