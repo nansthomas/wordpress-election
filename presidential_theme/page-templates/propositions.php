22e36c601ee857c5f92bdb171fbbb5114653c684
Clément Vion <clement.vion1@gmail.com>
2016-11-17 15:02:50 +0100

    display propositions and their taxonomies

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -2,15 +2,32 @@
 	/*
 	Template Name: Page propositions
 	*/
-?>
 
-<?php 
 $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
     if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
 	    echo '<ul>';
 	    foreach ( $terms as $term ) {
-	        echo '<li>' . $term->name . '</li>';
-	        var_dump($term);
+	        echo '<li class="proposition" domaine="' . $term->slug . '">' . $term->name . '</li>';
 	    }
 	    echo '</ul>';
 	}
+?>
+
+<div class="propositions">
+		
+	<?php 
+
+		$args = array( 'post_type' => "proposition" );
+
+		$the_query = new WP_Query( $args );
+
+		if ( $the_query->have_posts() ) {
+			while( $the_query->have_posts() ) {
+				$the_query->the_post();
+	    		the_title();
+			} 
+		}
+
+	?>
+
+</div>
