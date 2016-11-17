fbb21c5abe31dff662b7c78c57c7feac26befd94
Clément Vion <clement.vion1@gmail.com>
2016-11-17 17:39:27 +0100

    style propositions

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -36,8 +36,10 @@ $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
 				echo "<li class='propositionsList__item'>";
 
 				// Get the taxonomy for the post
+				echo '<span class="propositionsList__itemTaxo">';
 				the_terms( get_the_ID(), 'categorie-proposition');
-				
+				echo '</span>';
+
 	    		// Display proposition
 	    		the_title();
 	    		echo "</li>";
