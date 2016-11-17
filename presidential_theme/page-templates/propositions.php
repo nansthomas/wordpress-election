38540376fdafd9ff8372f43b90b58701a5d6b548
Clément Vion <clement.vion1@gmail.com>
2016-11-17 14:41:58 +0100

    display taxonomies

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -3,3 +3,12 @@
 	Template Name: Page propositions
 	*/
 ?>
+
+<?php $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
+    if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+	    echo '<ul>';
+	    foreach ( $terms as $term ) {
+	        echo '<li>' . $term->name . '</li>';
+	    }
+	    echo '</ul>';
+	}
