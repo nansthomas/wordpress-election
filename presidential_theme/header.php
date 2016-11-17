88335305eddea4e6801efdad082ecde59973b3c2
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-17 22:29:10 +0100

    added logo to the header

--- presidential_theme/header.php
+++ presidential_theme/header.php
@@ -16,6 +16,11 @@
     <body <?php body_class(); ?>>
             
             <header class="header">
+                <div id="header-image">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                       <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" width="150" height="150" />
+                    </a>
+                </div>
                 <div class="header__nav">
                     <?php 
                     $args=array(
