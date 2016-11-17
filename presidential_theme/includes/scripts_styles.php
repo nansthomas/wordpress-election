15f6338ca71a67c54ea9bf34f9ec825edc5ef12e
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-17 16:11:21 +0100

    added ajax on propositions

--- presidential_theme/includes/scripts_styles.php
+++ presidential_theme/includes/scripts_styles.php
@@ -21,5 +21,10 @@ wp_register_style( 'main_style', get_template_directory_uri() . '/styles/css/mai
 // appel du style dans la page
 wp_enqueue_style( 'main_style' );
 
+
+  wp_localize_script( 'main_script', 'ajaxurl', admin_url('admin-ajax.php'));
+
+}
+
 }
 add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
