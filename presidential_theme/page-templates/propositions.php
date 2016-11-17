8c4acb070877a962ad6901a7a0420cea02f201c2
Clément Vion <clement.vion1@gmail.com>
2016-11-17 14:57:48 +0100

    display propositions and their taxonomies

--- presidential_theme/page-templates/propositions.php
+++ presidential_theme/page-templates/propositions.php
@@ -4,11 +4,13 @@
 	*/
 ?>
 
-<?php $terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
+<?php 
+$terms = get_terms( 'categorie-proposition', 'orderby=count&hide_empty=0' );
     if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
 	    echo '<ul>';
 	    foreach ( $terms as $term ) {
 	        echo '<li>' . $term->name . '</li>';
+	        var_dump($term);
 	    }
 	    echo '</ul>';
 	}
