afd0b0f925e009b49c38e28927d2a1a6a4eec7af
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-18 00:01:24 +0100

    added social share sidebar

--- presidential_theme/includes/sidebars.php
+++ presidential_theme/includes/sidebars.php
@@ -21,3 +21,14 @@ register_sidebar( array(
     'before_title'  => '<h2 class="widgettitle">',
     'after_title'   => '</h2>' 
 )   );
+
+register_sidebar( array(
+    'name'          => __('Sidebar social share'),
+    'id'            => 'sidebar-social-share',
+    'description'   => '',
+    'class'         => '',
+    'before_widget' => '<li id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</li>',
+    'before_title'  => '<h2 class="widgettitle">',
+    'after_title'   => '</h2>' 
+)   );
