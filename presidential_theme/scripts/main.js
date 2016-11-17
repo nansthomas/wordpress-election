2e4e66ec81fb7b55a5640386bfc25ced7c9bb035
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-17 20:51:13 +0100

    finish ajax

--- presidential_theme/scripts/main.js
+++ presidential_theme/scripts/main.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function($)
 			},
 			function(response)
 			{
-				console.log($('.propositionsList__item')[0].css('height'));
+				console.log($('.propositionsList__item'));
 				var height = $('.propositionsList__item').length * parseInt($($('.propositionsList__item')[0]).css('height'));
 				console.log(height);
 				$('.propositionsList__items').append(response);
