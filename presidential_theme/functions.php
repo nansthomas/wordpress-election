d264a6c81f9d39d47a10d1212c3ba72ada30fe02
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-18 13:50:07 +0100

    added plugins dependency

--- presidential_theme/functions.php
+++ presidential_theme/functions.php
@@ -8,6 +8,7 @@ include_once ('includes/roles.php');
 include_once ('includes/excerpt.php');
 include_once ('includes/sidebars.php');
 include_once ('includes/taxonomies.php');
-include_once ('includes/color_scheme.php');
 include_once ('includes/woocommerce.php');
 include_once ('includes/custom_form.php');
+require_once ('includes/class-tgm-plugin-activation.php');
+include_once ('includes/tgm_plugins.php');
