546d9e5738afb391f8659f6af023536eb6be25ca
Arnaud Villani <arnaud.villani@gmail.com>
2016-11-18 01:29:15 +0100

    added dynamic link from home to shop

--- presidential_theme/views/flexible-parts/bande_projet.php
+++ presidential_theme/views/flexible-parts/bande_projet.php
@@ -3,6 +3,7 @@
 	$project_description_1 = get_sub_field('description_projet_1');
 	$project_description_2 = get_sub_field('description_projet_2');
 	$project_button_text = get_sub_field('texte_bouton_projet');
+	$project_button_link = get_sub_field('lien_bouton_projet');
 ?>
 
 <section class="homeProgram">
@@ -13,7 +14,7 @@
 			<?php echo $project_description_1; ?><br><br>
 			<?php echo $project_description_2; ?>
 		</p>
-		<a href="#" class="button">
+		<a href="<?php echo $" class="button">
 			<span><?php echo $project_button_text; ?></span>
 		</a>
 	</div>
